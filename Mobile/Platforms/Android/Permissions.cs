8a13dbb980dbac193072796341e9965f588e2b1b
nguyenkienvt9@gmail.com./DeKT.shgit config --global user.name nguyenkienvt9@gmail.com <nguyenkienvt9@gmail.com>
2026-03-19 22:51:23 +0700

    Add StartPage, map support, and Android location permissions
    Updated XML encoding in App.xaml and AppShell.xaml. Switched ShellContent to StartPage and adjusted routes/titles. Refactored MauiProgram.cs and added optional Maui Maps support. Added Microsoft.Maui.Controls.Maps package and StartPage.xaml compilation in csproj. Introduced Permissions.cs to declare Android location permissions for map features.

---
+++ Mobile/Platforms/Android/Permissions.cs
@@ -0,0 +1,6 @@
+﻿#if ANDROID
+using Android.App;
+
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+#endif
