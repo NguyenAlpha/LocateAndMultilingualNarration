2a059a9138f148c8339c2e5d0b232bc8ebc3c3d3
NhatNguyen <nhatnguyen27042005@gmail.com>
2026-04-18 23:50:43 +0700

    Remove unused using directives and simplify debug reset logic in HeaderView

--- Mobile/Components/HeaderView.xaml.cs
+++ Mobile/Components/HeaderView.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Mobile.Services;
-
+﻿
 namespace Mobile.Components;
 
 public partial class HeaderView : Grid
@@ -37,11 +35,8 @@ public partial class HeaderView : Grid
 #if DEBUG
     private async void OnDebugResetDeviceId(object? sender, EventArgs e)
     {
-        var deviceService = Handler?.MauiContext?.Services.GetRequiredService<IDeviceService>();
-        deviceService.ResetDeviceId();
-        var newId = deviceService.GetOrCreateDeviceId();
-        await Application.Current!.Windows[0].Page!.DisplayAlertAsync(
-            "Device ID Reset", $"New ID:\n{newId}", "OK");
+        Preferences.Clear();
+        await Shell.Current.GoToAsync("//LoadingPage");
     }
 #endif
 }
