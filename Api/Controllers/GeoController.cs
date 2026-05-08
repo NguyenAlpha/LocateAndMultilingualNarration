1d87186c41a6e405ba50f6f3b3be52b349da3a08
NhatNguyen <nhatnguyen27042005@gmail.com>
2026-05-08 09:10:20 +0700

    Nothing

--- Api/Controllers/GeoController.cs
+++ Api/Controllers/GeoController.cs
@@ -81,7 +81,7 @@ namespace Api.Controllers
 
             var summary = new ActiveDevicesSummaryDto
             {
-                ActiveCount   = devices.Count * 2,
+                ActiveCount   = devices.Count,
                 WithinSeconds = withinSeconds,
                 AsOf          = DateTimeOffset.UtcNow,       // timestamp server tạo response, dùng để hiển thị "cập nhật lúc"
                 Devices       = devices
